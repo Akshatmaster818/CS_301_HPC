03fd9efd7381f6ba8c0acbe887bb6602f45f1fb1
kaushik Prajapati <kaushikprajapati1100@gmail.com>
2026-01-20 23:09:53 +0530

    Initial PIC Lab: Performance Measurement for different benchmarking kernels

---
+++ utils.cpp
@@ -0,0 +1,18 @@
+#include <math.h>
+#include "utils.h"
+
+void vector_triad_operation(double *x, double *y, double *v, double *S, int Np) {
+
+    for (int p = 0; p < Np; p++) {
+        S[p] = x[p] + v[p] * y[p];
+
+        // Prevent compiler from optimizing away the loop
+        if (((double)p) == 333.333)
+            dummy(p);
+
+    }
+}
+
+void dummy(int x) {
+    x = 10 * sin(x / 10.0);
+}
